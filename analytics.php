42768dd6a4899eec10209b36e5d0826a13fb94d4
kruchyns <kruchyns@adobe.com>
2024-09-12 15:55:56 -0400

    Custom analytics for demo
    added analytics.php

--- analytics.php
+++ analytics.php
@@ -1,8 +1,31 @@
 <?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+function getUserIP() {
+    if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+        if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
+            $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+            return trim($addr[0]);
+        } else {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+if (!file_exists('access.log')) {
+    file_put_contents('access.log', '');
+}
+$txt = getUserIP() . " ". $_SERVER['REQUEST_METHOD'];
+file_put_contents('access.log', $txt.PHP_EOL , FILE_APPEND | LOCK_EX);
+
 $logFile = "data.log";
 if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
     echo "Deleted";
-    file_put_contents($logFile, '');
+    file_put_contents($logFile, "{}");
 }
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (!$_POST["event"]) {
@@ -34,21 +57,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             }
         }
     }
-?>
-<head>
-    <script src="https://cdn.anychart.com/releases/8.12.1/js/anychart-base.min.js" type="text/javascript"></script>
-</head>
-<body>
-<div id="container" style="width: 500px; height: 400px;"></div>
-</body>
-<script>
-    anychart.onDocumentLoad(function () {
-        var chart = anychart.pie();
-        chart.data([<?php echo $chartData; ?>]);
-        chart.title("Payment Services Extension Analytics");
-        chart.container("container");
-        chart.draw();
-    });
-</script>
-<?php
+    ?>
+    <head>
+        <script src="https://cdn.anychart.com/releases/8.12.1/js/anychart-base.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+    <div id="container" style="width: 800px; height: 600px;"></div>
+    </body>
+    <script>
+        anychart.onDocumentLoad(function () {
+            var chart = anychart.pie();
+            chart.data([<?php echo $chartData; ?>]);
+            chart.title("Payment Services Extension Analytics");
+            chart.container("container");
+            chart.draw();
+        });
+    </script>
+    <?php
 }
